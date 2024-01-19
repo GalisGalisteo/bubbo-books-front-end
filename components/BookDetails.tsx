48e4cc4a946d4c54ca621ebe7e6b39b56f4e5f27
Galis <galis.travelling@gmail.com>
2024-01-19 22:25:02 +0100

    preparing form for edit and view

--- components/BookDetails.tsx
+++ components/BookDetails.tsx
@@ -25,7 +25,28 @@ export const BookDetails = ({
   setSelectedBookId,
 }: BookDetailsProps) => {
   const [bookDetails, setBookDetails] = useState<null | Book>(null);
-  const [editable, setEditable] = useState(true);
+  const [editable, setEditable] = useState(false);
+
+  const [inputs, setInputs] = useState({
+    author: "",
+    title: "",
+    summary: "",
+    yearPublished: "",
+    genre: "",
+    isbn: "",
+  });
+
+  const updateInput = (name: string, value: string) => {
+    setInputs((prevInputs) => ({
+      ...prevInputs,
+      [name]: value,
+    }));
+  };
+
+  //   const [inputAuthor, setInputAuthor] = useState("");
+  //   const [inputTitle, setInputTitle] = useState("");
+  //   const [inputSummary, setInputSummary] = useState("");
+  //   const [inputYearPublished, setInputYearPublished] = useState("");
 
   const fetchBookDetails = async (id: string | null) => {
     try {
@@ -85,47 +106,103 @@ export const BookDetails = ({
         }}
         style={styles.image}
       />
-      <TextInput 
-      editable={editable}
-      
-      />
-
-      <Text>Id: {id}</Text>
-      <Text>Author: {author}</Text>
-      <Text>Title: {title}</Text>
-      <Text>Id: {id}</Text>
-      <Text>Author: {author}</Text>
-      <Text>Title: {title}</Text>
-      <View style={{ flexDirection: "row" }}>
-        <Button
-          title="Edit"
-          onPress={() => {
-            handleDeleteBook(selectedBookId);
-            setSelectedBookId();
-          }}
-        />
-        <Button
-          title="Delete"
-          onPress={() => {
-            Alert.alert("Deleting Book", "Are you sure you want to delete?", [
-              {
-                text: "Yes",
-                onPress: () => {
-                  handleDeleteBook(selectedBookId);
-                  setSelectedBookId();
-                },
-              },
-              {
-                text: "No",
-                style: "cancel",
-                onPress: () => {
-                  console.log("delete cancelled");
-                },
-              },
-            ]);
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputTitle}>Author: </Text>
+        <TextInput
+          editable={editable}
+          style={isEditable(editable)}
+          defaultValue={author}
+          onChangeText={(input) => {
+            updateInput("author", input);
           }}
         />
       </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputTitle}>Title: </Text>
+        <TextInput
+          editable={editable}
+          style={isEditable(editable)}
+          defaultValue={title}
+          onChangeText={(input) => {
+            updateInput("title", input);
+          }}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputTitle}>Summary: </Text>
+        <TextInput
+          editable={editable}
+          style={isEditable(editable)}
+          defaultValue={summary}
+          onChangeText={(input) => {
+            updateInput("summary", input);
+          }}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputTitle}>Year published: </Text>
+        <TextInput
+          editable={editable}
+          style={isEditable(editable)}
+          defaultValue={yearPublished}
+          onChangeText={(input) => {
+            updateInput("yearPublished", input);
+          }}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputTitle}>Genre: </Text>
+        <TextInput
+          editable={editable}
+          style={isEditable(editable)}
+          defaultValue={genre}
+          onChangeText={(input) => {
+            updateInput("genre", input);
+          }}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputTitle}>ISBN: </Text>
+        <TextInput
+          editable={editable}
+          style={isEditable(editable)}
+          defaultValue={isbn}
+          onChangeText={(input) => {
+            updateInput("isbn", input);
+          }}
+        />
+      </View>
+
+      {!editable ? (
+        <View style={{ flexDirection: "row" }}>
+          <Button title="Edit" onPress={() => setEditable(true)} />
+          <Button
+            title="Delete"
+            onPress={() => {
+              Alert.alert("Deleting Book", "Are you sure you want to delete?", [
+                {
+                  text: "Yes",
+                  onPress: () => {
+                    handleDeleteBook(selectedBookId);
+                    setSelectedBookId();
+                  },
+                },
+                {
+                  text: "No",
+                  style: "cancel",
+                  onPress: () => {
+                    console.log("delete cancelled");
+                  },
+                },
+              ]);
+            }}
+          />
+        </View>
+      ) : (
+        <View>
+          <Button title="Submit" onPress={() => {}} />
+        </View>
+      )}
     </View>
   );
 };
@@ -144,4 +221,27 @@ const styles = StyleSheet.create({
   details: {
     fontSize: 20,
   },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  inputTitle: {
+    fontSize: 18,
+  },
+  inputText: {
+    borderRadius: 10,
+    padding: 8,
+    width: "auto",
+    fontSize: 18,
+  },
+  borderEditable: {
+    borderWidth: 1,
+    borderColor: "#999",
+  },
 });
+
+const isEditable = (editable: boolean) => {
+  return editable
+    ? [styles.inputText, styles.borderEditable]
+    : [styles.inputText];
+};
